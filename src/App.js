8200d3d9287d925a9795523e2c1aaf944af1772a
Juratbekdasturchi <juratbekgithubabduraimov@gmail.com>
2024-03-12 13:11:12 +0500

    Hello world

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import monitor from './Monitor.svg';
 import telefon from './Telefon.svg';
-import rasmbir from '.'
+import rasmbir from './rasmbir.jpg';
+import rasmikki from './rasmikki.jpg';
+import rasmuch from './rasmuch.jpg';
+import rasmtort from './rasmtort.jpg';
+import rasmbesh from './rasmbesh.jpg';
+import rasmolti from './rasmolti.jpg';
+import rasmyetti from './rasmyetti.jpg';
+import rasmsakkiz from './rasmsakkiz.jpg';
+import rasmtoqqiz from './rasmtoqqiz.jpg';
+
 import './App.css';
 
 function App() {
@@ -28,15 +37,15 @@ function App() {
         </div>
 
 <div className="rasmlar">
-          <img src="https://static.wixstatic.com/media/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg/v1/fill/w_526,h_526,al_c,lg_1,q_80,enc_auto/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg" alt="" />
-          <img src="https://static.wixstatic.com/media/d78df6_9f4f79dd27814710b261e3acb931156e.jpg/v1/fill/w_351,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_9f4f79dd27814710b261e3acb931156e.jpg" alt="" />
-          <img src="https://static.wixstatic.com/media/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg" alt="" />
-          <img src="https://static.wixstatic.com/media/d78df6_3eea4c192eed4667a602ead5652459c5.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_3eea4c192eed4667a602ead5652459c5.jpg" alt="" />
-          <img src="https://static.wixstatic.com/media/d78df6_afe9335eb67640c988d648f7671308e5.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_afe9335eb67640c988d648f7671308e5.jpg" alt="" />
-          <img src="https://static.wixstatic.com/media/d78df6_b572be78b8984e2db58978eb21e433d7.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_b572be78b8984e2db58978eb21e433d7.jpg" alt="" />
-          <img src="https://static.wixstatic.com/media/d78df6_b9747effc3074458a295cf5674f8c73c.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_b9747effc3074458a295cf5674f8c73c.jpg" alt="" />
-          <img src="https://static.wixstatic.com/media/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg" alt="" />
-          <img src="https://static.wixstatic.com/media/d78df6_394a136048754355ad8432b30522c761.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_394a136048754355ad8432b30522c761.jpg" alt="" />
+          <img src={rasmbir} alt="" />
+          <img src={rasmikki} alt="" />
+          <img src={rasmuch} alt="" />
+          <img src={rasmtort} alt="" />
+          <img src={rasmbesh} alt="" />
+          <img src={rasmolti} alt="" />
+          <img src={rasmyetti} alt="" />
+          <img src={rasmsakkiz} alt="" />
+          <img src={rasmtoqqiz} alt="" />
 </div>
 
       </main>
